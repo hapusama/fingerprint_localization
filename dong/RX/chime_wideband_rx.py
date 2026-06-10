0a2e56cd5c862bd4ea64d06bd7eafbf03d7dee8a
hapusama <230259135@seu.edu.cn>
2026-06-10 10:19:04 +0800

    流程已通

--- dong/RX/chime_wideband_rx.py
+++ dong/RX/chime_wideband_rx.py
@@ -10,66 +10,36 @@
 # Description: Low-rate raw IQ receiver for Chime-style chirp sounding
 # GNU Radio version: 3.10.12.0
 
-from PyQt5 import Qt
-from gnuradio import qtgui
 from gnuradio import blocks
 from gnuradio import gr
 from gnuradio.filter import firdes
 from gnuradio.fft import window
 import sys
 import signal
-from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio import uhd
 import time
-import sip
 import threading
 
 
 
-class chime_wideband_rx(gr.top_block, Qt.QWidget):
+
+class chime_wideband_rx(gr.top_block):
 
     def __init__(self):
         gr.top_block.__init__(self, "Chime Wideband RX", catch_exceptions=True)
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("Chime Wideband RX")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except BaseException as exc:
-            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "chime_wideband_rx")
-
-        try:
-            geometry = self.settings.value("geometry")
-            if geometry:
-                self.restoreGeometry(geometry)
-        except BaseException as exc:
-            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
         self.flowgraph_started = threading.Event()
 
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 2000000
+        self.samp_rate = samp_rate = 20000000
         self.capture_seconds = capture_seconds = 10
         self.rx_gain_db = rx_gain_db = 20
         self.rx_device_addr = rx_device_addr = "serial=2603160"
-        self.rf_bandwidth = rf_bandwidth = 2000000
+        self.rf_bandwidth = rf_bandwidth = 20000000
         self.output_file = output_file = r"D:\Desktop\fingerprint_localization\dong\outputs\captures\chime_test_rx_fc32.bin"
         self.center_freq = center_freq = 487700000
         self.capture_samples = capture_samples = int(samp_rate * capture_seconds)
@@ -96,71 +66,17 @@ class chime_wideband_rx(gr.top_block, Qt.QWidget):
         self.uhd_usrp_source_0.set_bandwidth(rf_bandwidth, 0)
         self.uhd_usrp_source_0.set_rx_agc(False, 0)
         self.uhd_usrp_source_0.set_gain(rx_gain_db, 0)
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
-            (int(samp_rate * 0.025)), #size
-            samp_rate, #samp_rate
-            'RX Time Amplitude', #name
-            1, #number of inputs
-            None # parent
-        )
-        self.qtgui_time_sink_x_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0.set_y_axis(0, 1)
-
-        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
-
-        self.qtgui_time_sink_x_0.enable_tags(True)
-        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(True)
-        self.qtgui_time_sink_x_0.enable_grid(True)
-        self.qtgui_time_sink_x_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(True)
-        self.qtgui_time_sink_x_0.enable_stem_plot(False)
-
-
-        labels = ['|IQ| envelope', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
-            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ['blue', 'red', 'green', 'black', 'cyan',
-            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-        styles = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        markers = [-1, -1, -1, -1, -1,
-            -1, -1, -1, -1, -1]
-
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.blocks_complex_to_mag_0 = blocks.complex_to_mag(1)
+        self.blocks_head_0 = blocks.head(gr.sizeof_gr_complex*1, capture_samples)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, output_file, False)
+        self.blocks_file_sink_0.set_unbuffered(False)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_complex_to_mag_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_complex_to_mag_0, 0))
+        self.connect((self.blocks_head_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_head_0, 0))
 
-
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "chime_wideband_rx")
-        self.settings.setValue("geometry", self.saveGeometry())
-        self.stop()
-        self.wait()
-
-        event.accept()
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -168,7 +84,6 @@ class chime_wideband_rx(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.set_capture_samples(int(self.samp_rate * self.capture_seconds))
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
         self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
 
     def get_capture_seconds(self):
@@ -203,6 +118,7 @@ class chime_wideband_rx(gr.top_block, Qt.QWidget):
 
     def set_output_file(self, output_file):
         self.output_file = output_file
+        self.blocks_file_sink_0.open(self.output_file)
 
     def get_center_freq(self):
         return self.center_freq
@@ -216,35 +132,28 @@ class chime_wideband_rx(gr.top_block, Qt.QWidget):
 
     def set_capture_samples(self, capture_samples):
         self.capture_samples = capture_samples
+        self.blocks_head_0.set_length(self.capture_samples)
 
 
 
 
 def main(top_block_cls=chime_wideband_rx, options=None):
-
-    qapp = Qt.QApplication(sys.argv)
-
     tb = top_block_cls()
-
-    tb.start()
-    tb.flowgraph_started.set()
-
-    tb.show()
 
     def sig_handler(sig=None, frame=None):
         tb.stop()
         tb.wait()
 
-        Qt.QApplication.quit()
+        sys.exit(0)
 
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
 
-    timer = Qt.QTimer()
-    timer.start(500)
-    timer.timeout.connect(lambda: None)
+    tb.start()
+    tb.flowgraph_started.set()
 
-    qapp.exec_()
+    tb.wait()
+
 
 if __name__ == '__main__':
     main()
--- dong/TX/chime_wideband_tx.py
+++ dong/TX/chime_wideband_tx.py
@@ -10,8 +10,6 @@
 # Description: Low-rate Chime-style LFM upchirp transmitter using a repeated fc32 waveform file
 # GNU Radio version: 3.10.12.0
 
-from PyQt5 import Qt
-from gnuradio import qtgui
 from gnuradio import blocks
 import pmt
 from gnuradio import gr
@@ -19,60 +17,31 @@ from gnuradio.filter import firdes
 from gnuradio.fft import window
 import sys
 import signal
-from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio import uhd
 import time
-import sip
 import threading
 
 
 
-class chime_wideband_tx(gr.top_block, Qt.QWidget):
+
+class chime_wideband_tx(gr.top_block):
 
     def __init__(self):
         gr.top_block.__init__(self, "Chime Wideband TX", catch_exceptions=True)
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("Chime Wideband TX")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except BaseException as exc:
-            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "chime_wideband_tx")
-
-        try:
-            geometry = self.settings.value("geometry")
-            if geometry:
-                self.restoreGeometry(geometry)
-        except BaseException as exc:
-            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
         self.flowgraph_started = threading.Event()
 
         ##################################################
         # Variables
         ##################################################
-        self.tx_seconds = tx_seconds = 10
-        self.samp_rate = samp_rate = 2000000
         self.waveform_file = waveform_file = r"D:\Desktop\fingerprint_localization\dong\inputs\chime_test_tx_period_fc32.bin"
-        self.tx_samples = tx_samples = int(samp_rate * tx_seconds)
+        self.tx_seconds = tx_seconds = 20
         self.tx_gain_db = tx_gain_db = 100
         self.tx_device_addr = tx_device_addr = "serial=2512552"
-        self.rf_bandwidth = rf_bandwidth = 2000000
+        self.samp_rate = samp_rate = 20000000
+        self.rf_bandwidth = rf_bandwidth = 20000000
         self.center_freq = center_freq = 487700000
 
         ##################################################
@@ -97,92 +66,17 @@ class chime_wideband_tx(gr.top_block, Qt.QWidget):
         self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
         self.uhd_usrp_sink_0.set_bandwidth(rf_bandwidth, 0)
         self.uhd_usrp_sink_0.set_gain(tx_gain_db, 0)
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
-            (int(samp_rate * 0.025)), #size
-            samp_rate, #samp_rate
-            'TX Time Amplitude', #name
-            1, #number of inputs
-            None # parent
-        )
-        self.qtgui_time_sink_x_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0.set_y_axis(0, 0.3)
-
-        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
-
-        self.qtgui_time_sink_x_0.enable_tags(True)
-        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(True)
-        self.qtgui_time_sink_x_0.enable_grid(True)
-        self.qtgui_time_sink_x_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(True)
-        self.qtgui_time_sink_x_0.enable_stem_plot(False)
-
-
-        labels = ['TX |IQ| envelope', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
-            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ['blue', 'red', 'green', 'black', 'cyan',
-            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-        styles = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        markers = [-1, -1, -1, -1, -1,
-            -1, -1, -1, -1, -1]
-
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.blocks_head_0 = blocks.head(gr.sizeof_gr_complex*1, tx_samples)
+        self.blocks_head_0 = blocks.head(gr.sizeof_gr_complex*1, (int(samp_rate * tx_seconds)))
         self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, waveform_file, True, 0, 0)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
-        self.blocks_complex_to_mag_0 = blocks.complex_to_mag(1)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_complex_to_mag_0, 0), (self.qtgui_time_sink_x_0, 0))
         self.connect((self.blocks_file_source_0, 0), (self.blocks_head_0, 0))
-        self.connect((self.blocks_head_0, 0), (self.blocks_complex_to_mag_0, 0))
         self.connect((self.blocks_head_0, 0), (self.uhd_usrp_sink_0, 0))
 
-
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "chime_wideband_tx")
-        self.settings.setValue("geometry", self.saveGeometry())
-        self.stop()
-        self.wait()
-
-        event.accept()
-
-    def get_tx_seconds(self):
-        return self.tx_seconds
-
-    def set_tx_seconds(self, tx_seconds):
-        self.tx_seconds = tx_seconds
-        self.set_tx_samples(int(self.samp_rate * self.tx_seconds))
-
-    def get_samp_rate(self):
-        return self.samp_rate
-
-    def set_samp_rate(self, samp_rate):
-        self.samp_rate = samp_rate
-        self.set_tx_samples(int(self.samp_rate * self.tx_seconds))
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
-        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
 
     def get_waveform_file(self):
         return self.waveform_file
@@ -191,12 +85,12 @@ class chime_wideband_tx(gr.top_block, Qt.QWidget):
         self.waveform_file = waveform_file
         self.blocks_file_source_0.open(self.waveform_file, True)
 
-    def get_tx_samples(self):
-        return self.tx_samples
+    def get_tx_seconds(self):
+        return self.tx_seconds
 
-    def set_tx_samples(self, tx_samples):
-        self.tx_samples = tx_samples
-        self.blocks_head_0.set_length(self.tx_samples)
+    def set_tx_seconds(self, tx_seconds):
+        self.tx_seconds = tx_seconds
+        self.blocks_head_0.set_length((int(self.samp_rate * self.tx_seconds)))
 
     def get_tx_gain_db(self):
         return self.tx_gain_db
@@ -210,6 +104,14 @@ class chime_wideband_tx(gr.top_block, Qt.QWidget):
 
     def set_tx_device_addr(self, tx_device_addr):
         self.tx_device_addr = tx_device_addr
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.blocks_head_0.set_length((int(self.samp_rate * self.tx_seconds)))
+        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
 
     def get_rf_bandwidth(self):
         return self.rf_bandwidth
@@ -229,30 +131,22 @@ class chime_wideband_tx(gr.top_block, Qt.QWidget):
 
 
 def main(top_block_cls=chime_wideband_tx, options=None):
-
-    qapp = Qt.QApplication(sys.argv)
-
     tb = top_block_cls()
-
-    tb.start()
-    tb.flowgraph_started.set()
-
-    tb.show()
 
     def sig_handler(sig=None, frame=None):
         tb.stop()
         tb.wait()
 
-        Qt.QApplication.quit()
+        sys.exit(0)
 
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
 
-    timer = Qt.QTimer()
-    timer.start(500)
-    timer.timeout.connect(lambda: None)
+    tb.start()
+    tb.flowgraph_started.set()
 
-    qapp.exec_()
+    tb.wait()
+
 
 if __name__ == '__main__':
     main()
